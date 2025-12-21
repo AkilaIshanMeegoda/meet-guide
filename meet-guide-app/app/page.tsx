c419d2058c6ee6d4a954246f607aa89e7b78dfea
Akila Ishan Meegoda <akilaishanmeegoda@gmail.com>
2025-12-21 10:02:32 +0530

    update page.tsx code with new home page

--- meet-guide-app/app/page.tsx
+++ meet-guide-app/app/page.tsx
@@ -1,5 +1,28 @@
-export default function Home() {
+import React from "react";
+import Header from "@/components/home/Header";
+import HeroSection from "@/components/home/HeroSection";
+import FeaturesSection from "@/components/home/FeaturesSection";
+import HowItWorksSection from "@/components/home/HowItWorksSection";
+import BenefitsSection from "@/components/home/BenefitsSection";
+import StatsSection from "@/components/home/StatsSection";
+import TestimonialsSection from "@/components/home/TestimonialsSection";
+import CTASection from "@/components/home/CTASection";
+import Footer from "@/components/home/Footer";
+import ScrollToTop from "@/components/home/ScrollToTop";
+
+export default function HomePage() {
   return (
-      <h1 className="text-5xl text-center p-8 ">Hello Meet Guide</h1>
+    <div className="relative min-h-screen w-full overflow-x-hidden bg-white">
+      <Header />
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <BenefitsSection />
+      <StatsSection />
+      <TestimonialsSection />
+      <CTASection />
+      <Footer />
+      <ScrollToTop />
+    </div>
   );
 }
